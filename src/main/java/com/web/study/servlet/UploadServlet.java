526332d1bd2889494ecf260b4d881758419dd998
MB-study <MB-study@192.168.1.209>
2020-09-12 12:28:08 +0800

    javaweb

--- src/main/java/com/web/study/servlet/UploadServlet.java
+++ src/main/java/com/web/study/servlet/UploadServlet.java
@@ -2,13 +2,22 @@ package com.web.study.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.IOUtils;
 
 @WebServlet(urlPatterns = "/servlet/upload")
+@MultipartConfig(
+        location = "C:/upload",
+        fileSizeThreshold = 1024 * 1024 * 1,
+        maxFileSize = 1024 * 1024 * 10,
+        maxRequestSize = 1024 * 1024 * 20
+)
 public class UploadServlet extends HttpServlet {
 
     @Override
@@ -17,7 +26,29 @@ public class UploadServlet extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter out = resp.getWriter();
-        out.print("Upload OK");
+        // 分析 part (name = desc1)        
+        req.getParts().stream()
+                .filter(part -> part.getName().equals("desc1"))
+                .forEach(part -> {
+                    try {
+                        String desc1 = IOUtils.toString(part.getInputStream(), StandardCharsets.UTF_8.name());
+                        out.print(desc1);
+                    } catch (Exception e) {
+                    }
+                });
+        // 分析 part (name = myfile1)
+        req.getParts().stream()
+                .filter(part -> part.getName().equals("myfile1"))
+                .forEach(part -> {
+                    String fname = part.getSubmittedFileName();
+                    try {
+                        part.write(fname);
+                        out.print("Upload OK");
+                        out.print("<img width='500' src='/JavaWeb0829/servlet/image?name=" + fname + "'>");
+                    } catch (Exception e) {
+                        out.print("Upload Error" + e);
+                        e.printStackTrace(out);
+                    }
+                });
     }
-
 }
--- src/main/webapp/forms/upload_form.jsp
+++ src/main/webapp/forms/upload_form.jsp
@@ -7,14 +7,15 @@
         <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
     </head>
     <body style="padding: 20px">
-        <form method="post" class="pure-form" action="/JavaWeb0829/servlet/upload" encytpe="multipart/form-data">
+        <form method="post" class="pure-form" action="/JavaWeb0829/servlet/upload" enctype="multipart/form-data" target="myiframe">
             <fieldset>
                 <legend>Upload 上傳圖片</legend>
-                <input type="file"  name="myfile1" /><p />
-                <input type="text" placeholder="請輸入圖片名稱" name="desc1" /><p />       
+                <input type="file" name="myfile1" /><p />
+                <input type="text" placeholder="請輸入圖片名稱" name="desc1" /><p />
                 <button type="submit" class="pure-button pure-button-primary">上傳</button>
             </fieldset>
         </form>
-
+        <hr>
+        <iframe frameborder="0" name="myiframe" width="1000" height="1000"></iframe>
     </body>
 </html>
