c6794d7f8eea9e192bc12886fa82c1a4d8f5c49c
MB-study <MB-study@192.168.1.209>
2020-10-03 11:39:24 +0800

    javaweb

--- src/main/java/com/web/study/rest/MyApplication.java
+++ src/main/java/com/web/study/rest/MyApplication.java
@@ -11,9 +11,17 @@ public class MyApplication extends Application {
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> classes = new LinkedHashSet<>();
-        classes.add(HelloService.class);
+        classes.add(HelloService.class); // 建立新物件
         classes.add(BMIService.class);
+        //classes.add(CountSingleTonService.class);
         return classes; 
     }
-   
+
+    @Override
+    public Set<Object> getSingletons() {
+        Set<Object> set = new LinkedHashSet<>();
+        set.add(new CountSingleTonService());
+        return set;
+    }
+  
 }
