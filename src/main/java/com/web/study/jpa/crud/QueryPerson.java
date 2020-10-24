36b3fcb973f39353fd36643a9ed9fe214b40f8d9
MB-study <MB-study@192.168.1.209>
2020-10-24 12:06:20 +0800

    javaweb

--- src/main/java/com/web/study/jpa/crud/QueryPerson.java
+++ src/main/java/com/web/study/jpa/crud/QueryPerson.java
@@ -1,21 +1,20 @@
 package com.web.study.jpa.crud;
 
-import com.github.javafaker.Faker;
 import com.web.study.jpa.entity.Person;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/jpa/person/create")
-public class CreatePerson extends HttpServlet {
+@WebServlet("/jpa/person/query")
+public class QueryPerson extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -23,22 +22,31 @@ public class CreatePerson extends HttpServlet {
         resp.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter out = resp.getWriter();
-        // 建立 Person
-        Person person = new Person();
-        person.setName(new Faker().pokemon().name());
-        person.setAge(new Random().nextInt(10) + 10);
         // 取得 EntityManagerFactory
         EntityManagerFactory emf = (EntityManagerFactory)getServletContext().getAttribute("emf");
         // 取得 EntityManager
         EntityManager em = emf.createEntityManager();
-        // 新增一筆 Person 紀錄
-        // 建立交易模式
-        EntityTransaction et = em.getTransaction();
-        et.begin(); // 交易開始
-        em.persist(person); // 將 person 加入
-        et.commit(); // 交易提交
+        // 查詢全部資料
+        Query query = em.createQuery("SELECT p FROM Person p");
+        out.print(query.getResultList());
+        out.print("<hr>");
+        // 帶入條件查詢
+        String sql = "SELECT p FROM Person p WHERE p.age >= :age";
+        query = em.createQuery(sql);
+        query.setParameter("age", 15);
+        out.print(query.getResultList());
+        out.print("<hr>");
+        // 使用 NameQuery 1
+        List<Person> list = em.createNamedQuery("Person.findAll").getResultList();
+        out.print(list);
+        out.print("<hr>");
+        // 使用 NameQuery 2
+        list = em.createNamedQuery("Person.findByAge")
+                .setParameter("age", 15)
+                .getResultList();
+        out.print(list);
+        out.print("<hr>");
         em.close();
-        out.print(person + " 建立成功");
     }
     
 }
