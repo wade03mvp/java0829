800e896435b2b288c63fd70182596f0057e46e1b
MB-study <MB-study@192.168.1.209>
2020-10-31 16:24:36 +0800

    javaweb

--- src/main/java/com/web/websocket/HelloServer.java
+++ src/main/java/com/web/websocket/HelloServer.java
@@ -1,5 +1,7 @@
 package com.web.websocket;
 
+import java.util.Date;
+import java.util.concurrent.CopyOnWriteArraySet;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -8,17 +10,40 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/websocket/hello")
 public class HelloServer {
+
+    private static CopyOnWriteArraySet<Session> sessions;
+
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("進入 session: " + session.getId());
+        if (sessions == null) {
+            sessions = new CopyOnWriteArraySet<>();
+        }
+        sessions.add(session);
+        sendAll("歡迎 " + session.getId() + " 加入");
     }
+
     @OnMessage
-    public String onMessage(String message, Session session) {
-        System.out.println("id=" + session.getId() + " 說:" + message);
-        return "id=" + session.getId() + " 說:" + message;
+    public void onMessage(String message, Session session) {
+        System.out.println("id=" + session.getId() + " 說: " + message);
+        sendAll("id=" + session.getId() + " 說: " + message);
     }
+
     @OnClose
     public void onClose(Session session) {
-        System.out.println("關閉/離開 sesion: " + session.getId());
+        System.out.println("關閉/離開 session: " + session.getId());
+        if (sessions != null) {
+            sessions.remove(session);
+            sendAll("哇哩勒~ " + session.getId() + " 離開了!");
+        }
+    }
+
+    private void sendAll(String message) {
+        if (sessions != null) {
+            for (Session session : sessions) {
+                String time = "<font color='#aaaaaa' size='1'>" + new Date() + "</font>";
+                session.getAsyncRemote().sendText(message + " " + time);
+            }
+        }
     }
 }
